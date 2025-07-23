e08f0e0b41362f0fbba9427760dc19f68ea53977
Ashish_Sharma <121227516+Ashishhh4@users.noreply.github.com>
2025-07-23 15:42:55 +0530

    Time: 99 ms (80.27%), Space: 8.9 MB (22.92%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.c
@@ -0,0 +1,16 @@
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int i,j;
+    for( i=0;i<numsSize-1;i++){
+        for( j=i+1;j<numsSize;j++){
+            if(nums[i]+nums[j]==target){
+                int *res=(int *) malloc(2*sizeof(int));
+                res[0]=i;
+                res[1]=j;
+                *returnSize=2;
+                return res;
+            }
+        }
+    }   
+ *returnSize=0;
+ return NULL; 
+}
